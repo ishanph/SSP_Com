7490bb1b46c7144f0a481124966c8554b47f27ce
striker553 <52034505+striker553@users.noreply.github.com>
2020-02-09 12:13:03 +0530

    Update usart_transmit.c

--- usart_transmit.c
+++ usart_transmit.c
@@ -10,13 +10,13 @@ main (void)
 
   while (1)
     {
-      PORTC.DIRSET = 1 << 3;	//sets tx
-      PORTC.DIRCLR = 1 << 2;	//sets rx
+      PORTC.DIRSET = 0 b00000100;	//sets tx
+      PORTC.DIRCLR = 0 b00000010;	//sets rx
 
       USARTC0_BAUDCTRLA = 0 b00001100;	//sets baudrate
       USARTC0_BAUDCTRLB = 0 b00000000;	//seta bscale
 
-      USARTC0_CTRLB = 1 << 3;	//enable transmission
+      USARTC0_CTRLB = 0 b00000100;	//enable transmission
       USARTC0_CTRLC = 0 b00000011;	//set char length
 
       while (!(USARTC0_STATUS & 0 b00100000));	//waits for empty register
