2c6b6b026ecf4e6552a5e36e549ee5a43ab6a695
Ishan Phansalkar <52034505+ishanph@users.noreply.github.com>
2020-02-13 19:52:19 +0530

    Add files via upload
    Debugging pending

---
+++ spi_master.c
@@ -0,0 +1,69 @@
+//Master
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int tx = 4;
+
+void
+SPI_Init ()
+{
+  PORTD.DIR = 0 b10110000;  // MOSI, SCK, SS outputs MISO input
+  PORTD.OUTSET = 0 b00010000;   // sets SS pin high
+  SPID.CTRL = 0 b01010000;  // spi master, spi mode 0
+  SPID.INTCTRL = 0 b00000011;   // assign high priority to spid interrupts
+}
+
+void
+SPI_Trans (uint8_t dat)
+{
+  SPID_DATA = dat;
+  while (!(SPID_STATUS & 0x80)) // wait for transmit complete
+    {
+      PORTF_OUTSET = 0x02;  //LED at PF1 will glow
+      _delay_ms (500);
+    }
+  PORTF_OUTCLR = 0x02;
+  _delay_ms (500);
+}
+
+uint8_t
+SPI_Rec ()
+{
+  PORTF_OUTSET = 0x01;      //LED at PF1 will glow
+  _delay_ms (500);
+  uint8_t dat = SPID_DATA;  // grab received byte
+  while (!(SPID_STATUS & 0x80));    //wait till reception is complete
+  PORTF_OUTCLR = 0x01;
+  _delay_ms (500);
+  return dat;
+}
+
+int
+main (void)
+{
+  PORTF_DIRSET = 0xFF;
+  SPI_Init ();
+
+  PORTD.OUTCLR = 0 b00010000;   //pull ss low
+  sei ();           //global interrupts
+
+//tx = SPID.DATA; //recieve
+
+  while (1)
+    {
+      SPI_Trans (tx);
+      _delay_ms (10000);
+    }
+}
+
+ISR (SPID_INT_vect)
+{
+//tx = SPID.DATA; //store recieved
+//SPID.DATA = tx + 1; //increment and send back
+  _delay_ms (1500);
+  PORTF_OUT = 0 b10000000;
+  _delay_ms (1500);
+  PORTF_OUT = 0x00;
+}
---
+++ spi_slave.c
@@ -0,0 +1,82 @@
+//Slave
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+uint8_t rx = 0;
+
+void
+SPI_Init ()
+{
+  PORTC.DIR = 0 b01000000;  // MOSI, SCK, SS outputs MISO input
+  SPIC.CTRL = 0 b01000000;  // spi master, spi mode 0
+  SPIC.INTCTRL = 0 b00000011;   // assign high priority to spic interrupts
+  PMIC.CTRL = 0 b00000100;  // enable high priority interrupts
+}
+
+void
+SPI_Trans (uint8_t dat)
+{
+  SPIC_DATA = dat;
+  while (!(SPIC_STATUS & 0x80)) // wait for transmit complete
+    {
+      PORTF_OUTSET = 0x02;  //LED at PF1 will glow
+      _delay_ms (500);
+    }
+  PORTF_OUTCLR = 0x02;
+  _delay_ms (500);
+}
+
+uint8_t
+SPI_Rec ()
+{
+  PORTF_OUTSET = 0x01;      //LED at PF0 will glow
+  _delay_ms (500);
+// grab received byte
+  while (!(SPIC_STATUS & 0x80));    //wait till reception is complete
+  uint8_t dat = SPIC_DATA;
+  PORTF_OUTCLR = 0x01;
+  _delay_ms (500);
+  return dat;
+}
+
+int
+main (void)
+{
+  PORTF_DIRSET = 0xFF;
+  cli ();
+  sei ();           // global interrupts
+
+//SPIC.DATA = rx; //transmit
+
+  while (1)
+    {
+      SPI_Init ();
+//_delay_ms(1000);
+      PORTF_OUTSET = 0x08;
+      PORTF_OUTSET = 0x01;  //LED at PF0 will glow
+
+// grab received byte
+      while (!(SPIC_STATUS & 0x80));    //wait till reception is complete
+      rx = SPIC_DATA;
+      _delay_ms (500);
+      PORTF_OUTCLR = 0x01;
+      _delay_ms (500);
+      if (rx == 4)
+    PORTF_OUTSET = 0x80;
+//_delay_ms(1000);
+//PORTF_OUTCLR=0x01;
+    }
+}
+
+ISR (SPIC_INT_vect)
+{
+//rx = SPIC.DATA;     // store received
+//SPIC.DATA = rx;     // send back
+//rx = SPI_Rec();
+  _delay_ms (1000);
+  PORTF_OUTSET = 1 << (rx);
+  _delay_ms (1000);
+  PORTF_OUTCLR = 1 << (rx);
+}
