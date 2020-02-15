61c68b20ff9067996bb92e9c36d685f49a9ba473
Ishan Phansalkar <52034505+ishanph@users.noreply.github.com>
2020-02-15 17:28:19 +0530

    Update spi_master.c

--- spi_master.c
+++ spi_master.c
@@ -4,7 +4,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-int tx = 4;
+uint_8t tx = 4;
 
 void
 SPI_Init ()
@@ -13,6 +13,7 @@ SPI_Init ()
   PORTD.OUTSET = 0 b00010000;   // sets SS pin high
   SPID.CTRL = 0 b01010000;  // spi master, spi mode 0
   SPID.INTCTRL = 0 b00000011;   // assign high priority to spid interrupts
+  PMIC.CTRL = 0 b00000100;
 }
 
 void
@@ -55,16 +56,19 @@ main (void)
     {
       SPI_Trans (tx);
   //    _delay_ms (10000);
+      PORTF_OUT |= 1 << tx;
     }
 }
 
 ISR (SPID_INT_vect)
 {
   SPI_Rec(tx);
-  SPI_Trans(tx+1); //store recieved
+  tx++;
+  SPI_Trans(tx); //store recieved
 //SPID.DATA = tx + 1; //increment and send back
   //_delay_ms (1500);
   PORTF_OUT = 0 b10000000;
+  PORTF_OUT |= 1 << tx;
   //_delay_ms (1500);
-  PORTF_OUT = 0x00;
+  //PORTF_OUT = 0x00;
 }
